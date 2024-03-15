bd42421012237d16cf6b851d6140c3deae307cb1
侯佳奇 <houjiaqi@3dnest.cn>
2024-03-15 16:50:30 +0800

    feat:test result

--- test.py
+++ test.py
@@ -6,12 +6,15 @@ from aiohttp import ClientSession
 import pandas as pd
 
 DATA_CSV = Path('result').joinpath("data.csv")
-DATA_CSV.write_text("app_name,concurrency,total,max,min,mean,rps\n")
 
-HOST = "127.0.0.1"
+HOST = "192.168.8.83"
 FASTAPI = "fastapi"
 DJANGO = "django"
 PORTS = {FASTAPI: 8001, DJANGO: 8002}
+
+FA_TIME_IMG = Path('result').joinpath("fa_time.jpg")
+DJ_TIME_IMG = Path('result').joinpath("dj_time.jpg")
+RPS_IMG = Path('result').joinpath("rps.jpg")
 
 
 async def _request(session, url):
@@ -27,34 +30,59 @@ async def _request(session, url):
     return True, e - s
 
 
-async def request_app(app_name, loop=10, step=200):
+async def request_app(app_name, loop=20, step=500):
     url = f"http://{HOST}:{PORTS[app_name]}/test"
     f = DATA_CSV.open("a")
     async with ClientSession() as session:
         for l in range(1, loop + 1):
             concurrency = l * step
+            accum = 0
             mx = 0
             mn = 0
-            total = 0
             error = 0
             jobs = [_request(session, url) for _ in range(concurrency)]
-            for ok, t in await asyncio.gather(*jobs):
+            s = time.time()
+            results = await asyncio.gather(*jobs)
+            total = time.time() - s
+            for ok, t in results:
                 mx = max(mx, t)
                 mn = min(mn, t) if not mn == 0 else t
-                total += t
                 error += int(not ok)
+                accum += t
             rps = concurrency / total
-            mean = total / concurrency
-            f.write(f"{app_name},{concurrency},{total},{mx},{mn},{mean},{rps}\n")
+            mean = accum / concurrency
+            f.write(
+                f"{app_name},{concurrency},{total},{mx},{mn},{mean},{rps},{error}\n"
+            )
     f.close()
 
 
 async def main():
+    DATA_CSV.write_text("app_name,concurrency,total,max,min,mean,rps,error\n")
     await request_app(FASTAPI)
     await request_app(DJANGO)
 
 
+s = time.time()
 asyncio.run(main())
+print(time.time() - s)
 
-df = pd.read_csv(DATA_CSV.as_posix())
-print(df)
+FIG_SIZE = (12, 8)
+
+# 统计
+df_result = pd.read_csv(DATA_CSV.as_posix())
+print(df_result)
+print("RPS:", df_result["rps"].mean())
+
+df_result_concurrent_index = df_result.set_index(["app_name", "concurrency"])
+print(df_result_concurrent_index)
+df_result_concurrent_index.loc[FASTAPI][["max", "min", "mean", "total"]].plot(
+    figsize=FIG_SIZE,
+    xticks=df_result["concurrency"]).get_figure().savefig(FA_TIME_IMG)
+df_result_concurrent_index.loc[DJANGO][["max", "min", "mean", "total"]].plot(
+    figsize=FIG_SIZE,
+    xticks=df_result["concurrency"]).get_figure().savefig(DJ_TIME_IMG)
+df_result[["app_name", "concurrency",
+           "rps"]].set_index(["concurrency", "app_name"]).unstack().plot(
+               figsize=FIG_SIZE,
+               xticks=df_result["concurrency"]).get_figure().savefig(RPS_IMG)
